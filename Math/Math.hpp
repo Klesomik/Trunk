26f397664a79d3dd2af508217e0732d9e59f7448
Klesomik <neon_76@mail.ru>
2017-03-05 23:36:48 +0300

    Added some in Math
    GCD, LCM

--- Math/Math.hpp
+++ Math/Math.hpp
@@ -1,6 +1,29 @@
 #include <bits/stdc++.h>
 
+int Mod (int a, int b);
+int GCD (int a, int b);
+int LCM (int a, int b);
+
 int Mod (int a, int b)
 {
     return (a % b + b) % b;
+}
+
+int GCD (int a, int b)
+{
+    while (b != 0)
+    {
+        int tmp1 = b,
+            tmp2 = Mod (a, b);
+
+        a = tmp1;
+        b = tmp2;
+    }
+
+    return abs (a);
+}
+
+int LCM (int a, int b)
+{
+    return a / GCD (a, b) * b;
 }
